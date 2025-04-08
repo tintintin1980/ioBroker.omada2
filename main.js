3bc0ba41bfd8800e9cde11b1e09d488b477c0fde
tintintin1980 <102257731+tintintin1980@users.noreply.github.com>
2025-04-08 22:26:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -432,6 +432,7 @@ class Omada extends utils.Adapter {
     if (state) {
       if (!state.ack) {
         const idArray = id.split('.');
+        this.log.debug(idArray);
         const siteId = idArray[2];
         // const folder = idArray[3];
         if (idArray[3] === 'ssids') {
